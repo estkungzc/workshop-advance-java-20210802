697e41e5d9220848d8b642e1ae448f8b7f1e7bbe
KBANKNET\chairat.p <chairat.p@kbtg.tech>
2021-08-03 14:18:36 +0700

    test double: add demo spy

--- demo01/src/test/java/testdouble/GenerateIdServiceTest.java
+++ demo01/src/test/java/testdouble/GenerateIdServiceTest.java
@@ -18,6 +18,33 @@ class GenerateIdServiceTest {
         assertEquals("XYZ9", id);
     }
 
+    @Test
+    @DisplayName("[DEMO SPY] Called nextInt()")
+    public void case02() {
+        GenerateIdService service = new GenerateIdService();
+        SpyRandom spyRandom = new SpyRandom();
+        service.setRandom(spyRandom);
+        service.get();
+        spyRandom.verify(1);
+    }
+
+}
+
+class SpyRandom extends Random {
+
+    private int counter = 0;
+
+    @Override
+    public int nextInt(int bound) {
+        this.counter++;
+        return 1000;
+    }
+
+    public void verify(int expectedCount) {
+        if (expectedCount != counter) {
+            fail("จำนวนการเรียกใช้งาน method nextInt() ไม่ถูกต้อง");
+        }
+    }
 }
 
 class StubRandom extends Random {
